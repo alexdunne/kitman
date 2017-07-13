b02c02003e775230042b943dbd8286d9825d5ca6
Alex Dunne <hi@alexdunne.net>
2017-07-13 07:26:17 +0100

    Added an edit button to the ingredient show route

--- resources/views/ingredients/show.blade.php
+++ resources/views/ingredients/show.blade.php
@@ -3,6 +3,13 @@
 @section('content')
     <div class="container">
         <div class="row">
+            <div class="col-xs-12">
+                <a href="{{ route('ingredients.edit', $ingredient->id) }}" class="btn btn-primary pull-right">
+                    Edit ingredient
+                </a>
+            </div>
+        </div>
+        <div class="row">
             <div class="col-md-8 col-xs-12">
                 {{ $ingredient->name }}
             </div>
